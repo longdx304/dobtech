f7c6d34c597b681cb83cdc249a10b6b5a8c47e9d
Nam Nguyen <ducnam1719@gmail.com>
2024-05-24 17:08:25 +0700

    chore: styling component & image in notfound

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,26 +1,34 @@
 import { cn } from '@/lib/utils';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { ConfigProvider } from 'antd';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import theme from 'theme';
 import Favicon from '/public/images/Metadata/favicon.ico';
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:8000';
 
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: "CHAMDEP VN | Giày dép nam nữ trẻ em",
-	description: "Giày dép nam nữ trẻ em",
+  title: 'CHAMDEP VN | Giày dép nam nữ trẻ em',
+  description: 'Giày dép nam nữ trẻ em',
   metadataBase: new URL(BASE_URL),
-	icons: [{ rel: 'icon', url: Favicon.src }],
+  icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang='en' data-mode='light'>
-			<head>
-				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-			</head>
+      <head>
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+      </head>
+
       <body className={cn(font.className, 'w-full')}>
-        <main className="relative">{props.children}</main>
+        <AntdRegistry>
+          <ConfigProvider theme={theme}>
+            <main className='relative'>{props.children}</main>
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
