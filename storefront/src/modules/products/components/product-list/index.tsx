5a14259732d60883719e1805c48a33ec3196ff64
thienduyy <nguyenvthienduy@gmail.com>
2024-05-28 11:47:09 +0700

    fix: scroll to bottom fetch more product

--- storefront/src/modules/products/components/product-list/index.tsx
+++ storefront/src/modules/products/components/product-list/index.tsx
@@ -1,84 +1,102 @@
-'use client';
-import { Empty, Spin } from 'antd';
-import { useProducts } from 'medusa-react';
-import { FC, useEffect, useRef, useState } from 'react';
+"use client";
+import { Empty, Spin } from "antd";
+import { useProducts } from "medusa-react";
+import { FC, useEffect, useRef, useState } from "react";
+import _ from 'lodash';
 
-import { Flex } from '@/components/Flex';
-import ProductPreview from '@/modules/products/components/product-preview';
-import { ProductPreviewType } from '@/types/product';
+import { Flex } from "@/components/Flex";
+import ProductPreview from "@/modules/products/components/product-preview";
+import { ProductPreviewType } from "@/types/product";
+import { Text } from "@/components/Typography";
 
 interface ProductListProps {
-  data: {
-    products: ProductPreviewType[];
-    count: number;
-  };
-  searchValue?: string | null;
+	data: {
+		products: ProductPreviewType[];
+		count: number;
+	};
+	searchValue?: string | null;
 }
 
-const PAGE_SIZE = 6;
+const PAGE_SIZE = 10;
 const ProductList: FC<ProductListProps> = ({ data, searchValue = null }) => {
-  const ref = useRef(null);
-  const [productData, setProductData] = useState<ProductPreviewType[]>(
-    data?.products || []
-  );
-  const [pageNum, setPageNum] = useState(1);
-  const { products, isLoading, refetch, isRefetching, count } = useProducts({
-    q: searchValue || undefined,
-    limit: PAGE_SIZE,
-    offset: (pageNum - 1) * PAGE_SIZE,
-  });
+	const observerTarget = useRef(null);
+	const [productData, setProductData] = useState<ProductPreviewType[]>(
+		data?.products || []
+	);
+	const [pageNum, setPageNum] = useState(1);
+	const { products, isLoading, refetch, isRefetching, count } = useProducts({
+		q: searchValue || undefined,
+		limit: PAGE_SIZE,
+		offset: (pageNum - 1) * PAGE_SIZE,
+	});
 
-  const [pagingEnd, setPagingEnd] = useState(false);
+	const [pagingEnd, setPagingEnd] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrolledTo = window.scrollY + window.innerHeight;
-      const isReachBottom = document.body.scrollHeight === scrolledTo;
-      if (isReachBottom && productData?.length < count!) {
-        setPageNum((prev) => prev + 1);
-      }
-    };
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				if (entries[0].isIntersecting && !pagingEnd) {
+					setPageNum((prev) => prev + 1);
+				}
+			},
+			{ threshold: 1 }
+		);
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [productData, count]);
+		if (observerTarget.current) {
+			observer.observe(observerTarget.current);
+		}
 
-  useEffect(() => {
-    if (pageNum > 1 && products?.length) {
-      setProductData((prev) => [...prev, ...products] as ProductPreviewType[]);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [products]);
+		return () => {
+			if (observerTarget.current) {
+				observer.unobserve(observerTarget?.current);
+			}
+		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [observerTarget, pagingEnd]);
 
-  useEffect(() => {
-    setProductData(data?.products || []);
-  }, [data]);
-  return (
-    <div className='flex flex-col items-center gap-4'>
-      <h2>Sản phẩm mới</h2>
-      {productData?.length > 0 && (
-        <div
-          className='grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 max-sm:grid-cols-2 sm:grid-cols-2 w-full gap-x-6 gap-y-6'
-          ref={ref}
-        >
-          {productData?.map((product) => (
-            <ProductPreview key={product.id} data={product} />
-          ))}
-        </div>
-      )}
-      {(isLoading || isRefetching) && (
-        <Flex justify='center' align='center'>
-          <Spin size='large' />
-        </Flex>
-      )}
-      {count === 0 && (
-        <Empty
-          image={Empty.PRESENTED_IMAGE_SIMPLE}
-          description='Không có sản phẩm'
-        />
-      )}
-    </div>
-  );
+	useEffect(() => {
+		if (pageNum > 1 && products?.length) {
+			setProductData(
+				(prev) => [...prev, ...products] as ProductPreviewType[]
+			);
+		}
+		if (count === productData?.length) {
+			setPagingEnd(true);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [products, count]);
+
+	useEffect(() => {
+		setProductData(data?.products || []);
+	}, [data]);
+	return (
+		<div className="flex flex-col items-center gap-4">
+			{productData?.length > 0 && (
+				<div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 max-sm:grid-cols-2 sm:grid-cols-2 w-full gap-x-6 gap-y-6">
+					{productData?.map((product, index) => (
+						<ProductPreview key={index} data={product} />
+					))}
+				</div>
+			)}
+			{pagingEnd && (
+				<Flex justify="center" align="center">
+					<Text type="secondary">Không còn sản phẩm</Text>
+				</Flex>
+			)}
+			{(isLoading || isRefetching) && (
+				<Flex justify="center" align="center">
+					<Spin size="large" />
+				</Flex>
+			)}
+			{count === 0 && (
+				<Empty
+					image={Empty.PRESENTED_IMAGE_SIMPLE}
+					description="Không có sản phẩm"
+				/>
+			)}
+			<div ref={observerTarget} />
+		</div>
+	);
 };
 
 export default ProductList;
